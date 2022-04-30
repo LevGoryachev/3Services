277517a066f59690c6e3f1a36e9bea32c288976d
LevGoryachev <l.goryachev@inbox.ru>
2022-04-30 05:19:57 +0300

    OrderService dao

---
+++ OrderService/src/main/java/ru/goryachev/orderservice/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package ru.goryachev.orderservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.goryachev.orderservice.model.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
